38bfee85d1857b9c33fa9a7a557470d45ca896cc
EduardoTomoike <dudu.canetinha@hotmail.com>
2024-11-26 13:00:49 -0600

    models.py updated

--- srs/stockweb/models.py
+++ srs/stockweb/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 # Create your models here.
 class CustomerUser(AbstractUser):
+    user_ID = models.BigAutoField(auto_created=True, primary_key=True, serialize=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     security_question = models.CharField(max_length=50)
+
+
 
