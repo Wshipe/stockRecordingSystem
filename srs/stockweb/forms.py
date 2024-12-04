76287ed5172869c362c8cb8914bd6fc818596c53
Julio Aguero <142344773+JulioA9@users.noreply.github.com>
2024-12-04 00:03:58 -0600

    made changes to authentication, cahnged customer user to user

--- srs/stockweb/forms.py
+++ srs/stockweb/forms.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from django import forms
-from django.forms.widgets import TextInput,PasswordInput
 from .models import Note
 from .models import WatchList
 from .models import WatchListStock
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms.widgets import TextInput, PasswordInput
+from .models import Note, WatchList, WatchListStock
 
 
 class CreateUserForm(UserCreationForm):
